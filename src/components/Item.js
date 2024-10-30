f51f4bc74577924d827e6389c3ef7f08d5d5a25c
SushanThakur <mail@sushant.name.np>
2024-10-30 11:01:39 +0545

    Basic Structure

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import ItemCard from "./ItemCard"
+
+export default function Item(){
+  return(
+    <div className="card-container">
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+    </div>
+  )
+}
---
+++ src/components/ItemCard.js
@@ -0,0 +1,7 @@
+export default function ItemCard(){
+  return(
+    <div className="item-card scaleUp">
+      <h2>Test</h2>
+    </div>
+  )
+}
